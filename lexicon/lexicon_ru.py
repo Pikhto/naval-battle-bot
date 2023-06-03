ad03f93601e8229a57b7682aa1109d2178018695
Andrey Kuzmin <andromed-ku@yandex.ru>
2023-06-03 18:34:12 +0500

    add COMMANDS_RU, SERVICE_REPORTS

---
+++ lexicon/lexicon_ru.py
@@ -0,0 +1,8 @@
+COMMANDS_RU: dict[str, str] = {'/support': 'Поддержка',
+                               "/help": "Справка по работе бота",
+                               "/contacts": "Другие способы связи",
+                               }
+
+SERVICE_REPORTS: dict[str, str] = {
+    'in development': 'Still in development',
+    }
